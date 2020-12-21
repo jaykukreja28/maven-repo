f62877a2bcf0121cfe078a2265e427b857d94872
Sir Jay Kukreja <chunty@bunty.com>
2020-12-21 15:11:48 +0530

    third commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -2,17 +2,21 @@ package com.lti;
 
 public class Calc {
 
-	public static int add(int num1,int num2){
-		return num1+num2;
+	public static int add(int num1, int num2) {
+		return num1 + num2;
 	}
-	
-	public static int sub(int num1,int num2){
-		return num1-num2;
+
+	public static int sub(int num1, int num2) {
+		return num1 - num2;
 	}
-	
+
+	public static int mul(int num1, int num2) {
+		return num1 * num2;
+	}
+
 	public static void main(String[] args) {
-		
-		System.out.println(sub(10,20));
+
+		System.out.println(mul(10, 20));
 	}
 
 }
