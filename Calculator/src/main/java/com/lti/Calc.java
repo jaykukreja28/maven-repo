2dad697561acebca4032d1a1621cc9f3f8e2ae8c
Sir Jay Kukreja <chunty@bunty.com>
2020-12-21 15:02:16 +0530

    second commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -6,11 +6,13 @@ public class Calc {
 		return num1+num2;
 	}
 	
-	
+	public static int sub(int num1,int num2){
+		return num1-num2;
+	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println(add(10,20));
+		System.out.println(sub(10,20));
 	}
 
 }
